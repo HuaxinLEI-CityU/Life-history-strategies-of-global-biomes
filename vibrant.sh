6be9a54b5bb09cc070f7bec73e555db2c85a826e
Huaxin LEI <166279633+HuaxinLEI-CityU@users.noreply.github.com>
2026-01-27 21:30:14 +0800

    vibrant.sh

--- vibrant.sh
+++ vibrant.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-#PBS -l nodes=5:ppn=8
-#PBS -o /disk/rdisk10/huaxinlei2/air_sample2/18-19-kraken2/data_txz/pier/vibrant_o
-#PBS -e /disk/rdisk10/huaxinlei2/air_sample2/18-19-kraken2/data_txz/pier/vibrant_e
-#PBS -N vibrant_pier
-#PBS -l walltime=96:00:00
-#PBS -m bea
-#PBS -M huaxin_l@163.com
 
 source deactivate
 conda deactivate
 conda activate vibrant
 
-cd /disk/rdisk10/huaxinlei2/air_sample2/18-19-kraken2/data_txz/pier/
 mkdir no_phage
 
-python /disk/rdisk10/huaxinlei2/tools/VIBRANT/VIBRANT_run.py -i /disk/rdisk10/huaxinlei2/air_sample2/18-19-kraken2/data_txz/pier/contig/all_pier.fasta -t 96 -folder no_phage/pier_vibrant
+python ./VIBRANT_run.py -i ${i}_assembly.fasta -t 96 -folder no_phage/${i}_vibrant
