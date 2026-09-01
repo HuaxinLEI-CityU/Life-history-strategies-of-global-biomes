7a183b00c5e85d3d5f3a54016063b0c0d047d46c
Huaxin LEI <166279633+HuaxinLEI-CityU@users.noreply.github.com>
2026-09-01 10:29:10 +0800

    Update Eukrep.sh

--- Eukrep.sh
+++ Eukrep.sh
@@ -2,5 +2,5 @@
 dir='file_list'
 for i in $dir;
 do
-EukRep -i ${i}_marine.fasta -o ${i}_marine_Eukaryote_output --prokarya ${i}_marine_no_euk.fasta
+EukRep -i ${i}.fasta -o ${i}_Eukaryote_output --prokarya ${i}_no_euk.fasta
 done
