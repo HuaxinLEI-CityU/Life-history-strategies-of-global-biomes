f592e3e353ec1fc4d1ebdc7e157e78064b2d4362
Huaxin LEI <166279633+HuaxinLEI-CityU@users.noreply.github.com>
2026-09-04 22:48:52 +0800

    Update AGS_calculation.sh

--- AGS_calculation.sh
+++ AGS_calculation.sh
@@ -6,3 +6,7 @@ for i in $dir
 do
 python ./run_microbe_census.py -n 100000000 -t 90 ${i}_no_euk_phage.gasta ${i}_ags.txt
 done
+
+
+###########
+###########${i}_no_euk_phage.gasta is the prokaryotic contig.
