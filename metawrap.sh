ab93166b04b5135f613e4d0476fad2d6dceef5d6
Huaxin LEI <166279633+HuaxinLEI-CityU@users.noreply.github.com>
2026-09-04 22:52:18 +0800

    Update metawrap.sh

--- metawrap.sh
+++ metawrap.sh
@@ -8,11 +8,13 @@ mkdir metawrap
 for i in $dir
 do
 
-#assembly
+###assembly with megahit (default)
 metawrap assembly -1 ${i}_clean_1.fastq -2 ${i}_clena_1.fastq -t 80 -o metawrap/${i}_assembly -m 1000
 
-#binning and refinement
+###binning with three tools
 metawrap binning -o metawrap/${i}_binning -m 1000 -t 64 -a metawrap/${i}_assembly/final_assembly.fasta --metabat2 --maxbin2 --concoct ${i}_clean_1.fastq ${i}_clena_1.fastq
+
+##bins refinement
 metawrap bin_refinement -m 1000 -o metawrap/${i}_refine -t 60 -m 1000 -A metawrap/${i}_binning/metabat2_bins/ -B metawrap/${i}_binning/maxbin2_bins/ -C metawrap/${i}_binning/concoct_bins/ -c 50 -x 10
 
 done
