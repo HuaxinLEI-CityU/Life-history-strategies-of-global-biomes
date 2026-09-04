470252476c4c38190652aa2d591698dc773da52e
Huaxin LEI <166279633+HuaxinLEI-CityU@users.noreply.github.com>
2026-09-04 22:50:48 +0800

    Update dRep.sh

--- dRep.sh
+++ dRep.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+##genome dereplication using dRep with default settings (95% ANI).
+
+##using checkM output as input to reduce running time.
+
 dRep dereplicate drep_merine -g all_marine_bins/*fa --genomeInfo marine_checkm_out.csv -p 32
+
 dRep dereplicate drep_surface -g all_surface_bins/*fa --genomeInfo surface_checkm_out.csv -p 32
+
 dRep dereplicate drep_soil -g all_soil_bins/*fa --genomeInfo soil_checkm_out.csv -p 32
+
 dRep dereplicate drep_AS -g all_AS_bins/*fa --genomeInfo AS_checkm_out.csv -p 32
